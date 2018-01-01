3e0f5ca8a723348cb90d6fec4cf4942f0601c36d
pitchforkviking <pitchforkviking@outlook.com>
2018-01-02 00:32:22 +0530

    Fourth Base ;)

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+import { HomePage } from '../home/home';
 
 @Component({
   templateUrl: 'game.html'
@@ -17,11 +20,13 @@ export class GamePage {
 
   public pass: number = 0;
   public mode: number = 0;
+  public turn: number = 2;
 
 
   public p2p: any = [
     {
       name: "Alex",
+      avatar: "assets/imgs/Alex.png",
       lastWord: "",
       index: 0,
       score: 0,
@@ -31,6 +36,7 @@ export class GamePage {
     },
     {
       name: "Zoey",
+      avatar: "assets/imgs/Zoey.png",
       lastWord: "",
       index: 1,
       score: 0,
@@ -44,6 +50,7 @@ export class GamePage {
   public a4p: any[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
   public player: string;
+  public avatar: string;
   public lastWord: string;
   public score: number = 0;
   public bin: any[] = []
@@ -53,6 +60,7 @@ export class GamePage {
 
   constructor(
     public navCtrl: NavController,
+    public alertCtrl: AlertController,
     public http: Http) {
 
       this.http.get('../../assets/files/dictionary.txt').subscribe(
@@ -72,29 +80,15 @@ export class GamePage {
     var randomIndex;
     while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
     return array;
   }
-
-  funique(array:any) {
-    var a = array.concat();
-    for(var i=0; i<a.length; ++i) {
-        for(var j=i+1; j<a.length; ++j) {
-            if(a[i] === a[j])
-                a.splice(j--, 1);
-        }
-    }
-    return a;
-  }
-
 
   f2p(){
     this.mode = 2;
@@ -104,6 +98,7 @@ export class GamePage {
     this.p2p[1].deck = this.a2p;
 
     this.player = this.p2p[0].name;
+    this.avatar = this.p2p[0].avatar;
     this.score = this.p2p[0].score;
     this.deck = this.p2p[0].deck;
 
@@ -118,14 +113,16 @@ export class GamePage {
   }
 
   fborrow(letter:any){
-    this.board.push(letter);
-    this.isPlaced = true;
-    this.borrow.splice(this.borrow.indexOf(letter),1);
-    --this.score;
+    if(this.board.length < this.turn){
+      this.board.push(letter);
+      this.isPlaced = true;
+      this.borrow.splice(this.borrow.indexOf(letter),1);
+      --this.score;
+    }
   }
 
   fboard(letter:any){
-    this.bin.push(letter);
+    this.deck.push(letter);
     this.isPlaced = true;
     this.board.splice(this.board.indexOf(letter),1);
   }
@@ -137,31 +134,50 @@ export class GamePage {
   }
 
   fdeck(letter:any){
-    this.board.push(letter);
-    this.isPlaced = true;
-    this.deck.splice(this.deck.indexOf(letter),1);
+    if(this.board.length < this.turn){
+      this.board.push(letter);
+      this.isPlaced = true;
+      this.deck.splice(this.deck.indexOf(letter),1);
+    }
   }
 
   fpass() {
 
-    this.p2p[this.pass % this.mode].lastWord = this.lastWord;
-    this.p2p[this.pass % this.mode].score = this.score;
-    this.p2p[this.pass % this.mode].deck = this.deck;
-    this.p2p[this.pass % this.mode].board = this.board;
-    this.p2p[this.pass % this.mode].bin = this.bin;
+    let index = this.pass % this.mode;
+
+    this.p2p[index].lastWord = this.lastWord;
+    this.p2p[index].score = this.score;
+    this.p2p[index].deck = this.deck;
+    this.p2p[index].board = this.board;
+    //this.p2p[index].bin = this.bin;
 
     ++this.pass;
+    index = this.pass % this.mode;
+
+    if(index === 0){
+      ++this.turn;
+    }
+
+    if(this.turn === 10){
+      let alert = this.alertCtrl.create({
+        title: this.p2p[0].score > this.p2p[1].score ? this.p2p[0].name : this.p2p[1].name,
+        subTitle: 'Winner, Winner. Chicken Dinner!',
+        buttons: ['OK']
+      });
+      alert.present();
+      this.navCtrl.push(HomePage)
+    }
 
     this.borrow = this.board;
-    this.player = this.p2p[this.pass % this.mode].name;
-    this.score = this.p2p[this.pass % this.mode].score;
-    this.deck = this.p2p[this.pass % this.mode].deck;
-    this.bin = this.p2p[this.pass % this.mode].bin;
-    this.board = this.p2p[this.pass % this.mode].board;
+    this.player = this.p2p[index].name;
+    this.avatar = this.p2p[index].avatar;
+    this.score = this.p2p[index].score;
+    this.deck = this.p2p[index].deck;
+    //this.bin = this.p2p[index].bin;
+    this.board = this.p2p[index].board;
   }
 
   fcheck(){
-    alert(this.board.concat(this.bin).length);
     this.word = this.board.join('').toLowerCase();
     this.isPlaced = false;
     if(this.word !== ''){
